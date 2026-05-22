50af9d8b3e6990e2715b9e7b7d4479b26c38c193
cocobwmr-ship-it <cocobwmr@gmail.com>
2026-05-22 12:41:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 // 部署完成后在网址后面加上这个，获取自建节点和机场聚合节点，/?token=auto或/auto或
 
 let mytoken = 'auto';
-let guestToken = ''; //可以随便取，或者uuid生成，https://1024tools.com/uuid
+let guestToken = 'roger'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
 let BotToken = ''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 let ChatID = ''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
@@ -13,7 +13,9 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://cfxr.eu.org/getSub
+vmess://YXV0bzphYjIwODc5OC1lYzU3LTRhYmEtZWQxMi1lZGIxODk5MTZlNzNAdWNuMi55aWVuZXJneXNtYXJ0aG9tZS5jb206NTU5MDI?path=/wscqyh&remarks=%E8%8B%B1%E5%9B%BDCN2-VM&obfs=websocket&tls=1&alterId=0
+vmess://YXV0bzphYjIwODc5OC1lYzU3LTRhYmEtZWQxMi1lZGIxODk5MTZlNzNAdWNuMi55aWVuZXJneXNtYXJ0aG9tZS5jb206NTU5MDI?path=/wscqyh&remarks=%E8%8B%B1%E5%9B%BDCN2-VM&obfs=websocket&tls=1&alterId=0
+
 `;
 
 let urls = [];
